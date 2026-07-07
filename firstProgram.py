fec2991531080535278757feecd8cd683f1d6fbc
shashwatrajhack <er.shashwatraj@gmail.com>
2026-07-07 10:54:33 +0530

    basics of python

--- firstProgram.py
+++ firstProgram.py
@@ -2,6 +2,11 @@ print("hello world ")
 
 name = "Shashwat"
 age = 23
+
+a = 9
+b = 8
+
+print(a+b)
 print (name)
 print(age)
 
---
+++ practice1.py
@@ -0,0 +1,10 @@
+a = int(input("Enter a number:"))
+b = int(input("Enter another number: "))
+sum = a+b
+
+print(sum)
+
+side = float(input("Enter side of a square:"))
+
+area = side*side
+print("Area of Square is: ",area)
