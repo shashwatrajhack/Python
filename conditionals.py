6766b767a52d2ffd380fef8885e17971e32cf139
shashwatrajhack <er.shashwatraj@gmail.com>
2026-07-08 09:07:52 +0530

    conditionals in python

---
+++ conditionals.py
@@ -0,0 +1,45 @@
+# age = 17
+
+# if(age >= 23):
+#     print("You are eligible to vote")
+# elif(age<18):
+#     print("You are not elegible to vote")
+# else:
+#     print("Do you have voter id card?")
+
+
+# marks = int(input("ENter your marks: "))
+
+# if(marks >= 90):
+#     print("A grade")
+# elif(marks >= 80):
+#     print("You got B grade")
+# elif(marks >= 70):
+#     print("you got C grade")
+# else:
+#     print("You failed")
+
+
+# number = int(input("Enter a number:"))
+
+# if(number%2 == 0):
+#     print("Even")
+# else:
+#     print("Odd")
+
+
+
+num1 = int(input("Enter the first number:"))
+
+num2 = int(input("Enter the second number:"))
+
+num3 = int(input("Enter the third number:"))
+
+if(num1>num2 and num1>num3):
+    print("The largest number is: ",num1)
+elif(num2>num1 and num2>num3):
+    print("The largest number is : ",num2)
+else:
+    print("The largest number is: ",num3)
+
+
--- strings.py
+++ strings.py
@@ -1,3 +1,12 @@
-s = "Muzaffarpur"
-type(s)
-print(s)
+#s = "Muzaffarpur"
+#type(s)
+#print(s)
+
+
+str1 = input("Enter your name: ")
+
+str2 = "hello $$$$$$ why are you so high"
+
+print(len(str1))
+
+print(str2.count("$"))
