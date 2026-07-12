f8ebe048fe4d81f20a6a464a5282b0da872bca5c
shashwatrajhack <er.shashwatraj@gmail.com>
2026-07-12 10:35:17 +0530

    Python class and self function

--- OOP/class.py
+++ OOP/class.py
@@ -1,9 +1,25 @@
 # Creating class and object in python
 
-class Student:
-    name="Karan"
+# class Student:
+#     name="Karan"
 
-s1 = Student()
+# s1 = Student()
 
-print(s1.name)
+# print(s1.name)
 
+class Employee:
+    def __init__(self,sal,ag):
+        self.salary = sal
+        self.age = ag
+
+    def display(self):
+        print(f"salary is {self.salary} and age is {self.age}");
+
+
+e1 = Employee(200000,23)
+e2 = Employee(100000,24)
+
+print(e1.salary,e1.age)
+print(e2.salary,e2.age)
+
+e2.display()
